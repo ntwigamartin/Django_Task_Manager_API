ae18b314256a7d30bb7cee9c56c6f7e19ba68e37
Ntwiga <ntwiga.martin@gmail.com>
2025-06-21 00:39:39 +0300

    Task model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = 'Low'
+        MEDIUM = 'Medium'
+        HIGH = 'High'
+
+    class Status(models.TextChoices):
+        CREATED = 'Created'
+        STARTED = 'Started'
+        COMPLETED = 'Completed'
+        CANCELLED = 'Cancelled'
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=3,
+        choices=Priority.choices,
+        default=Priority.LOW
+    )
+    status = models.CharField(
+        max_length=4,
+        choices=Status.choices,
+        default=Status.CREATED
+    )
+
+    def __str__(self):
+        return self.title
