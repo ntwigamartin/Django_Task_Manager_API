35115349e183e7858a0e688c27f26cd9f06e3043
Ntwiga <ntwiga.martin@gmail.com>
2025-06-23 14:48:06 +0300

    added django default restframework auth

--- api/views.py
+++ api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
--- py_task_manager/settings.py
+++ py_task_manager/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+# 6e94baa52c4de1477e54e72f5d88fc08be9de2e9
